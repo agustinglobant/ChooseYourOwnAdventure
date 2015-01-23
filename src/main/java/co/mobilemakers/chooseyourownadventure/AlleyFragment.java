69515057820b49657c89fc6b96e6a99d5189a86d
Agustín <agustin.gugliotta@globant.com>
2015-01-23 19:14:53 -0300

    Added random calls to room and alley in buttons

--- src/main/java/co/mobilemakers/chooseyourownadventure/AlleyFragment.java
+++ src/main/java/co/mobilemakers/chooseyourownadventure/AlleyFragment.java
@@ -6,18 +6,34 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class AlleyFragment extends Fragment {
-
+    Button mButtonGoLeft, mButtonGoRight, mButtonContinue;
 
     public AlleyFragment() {
         // Required empty public constructor
     }
 
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        getWidgets();
+        mButtonGoRight.setOnClickListener(new ChanceToARandomScreen(getActivity()));
+        mButtonGoLeft.setOnClickListener(new ChanceToARandomScreen(getActivity()));
+        mButtonContinue.setOnClickListener(new ChanceToARandomScreen(getActivity()));
+    }
+
+    private void getWidgets() {
+        mButtonContinue = (Button) getView().findViewById(R.id.button_continue);
+        mButtonGoLeft = (Button) getView().findViewById(R.id.button_go_left);
+        mButtonGoRight = (Button) getView().findViewById(R.id.button_go_right);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
--- src/main/java/co/mobilemakers/chooseyourownadventure/RoomFragment.java
+++ src/main/java/co/mobilemakers/chooseyourownadventure/RoomFragment.java
@@ -6,18 +6,32 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class RoomFragment extends Fragment {
-
+    Button mButtonDoorOne, mButtonDoorTwo;
 
     public RoomFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        getWidgets();
+        mButtonDoorOne.setOnClickListener(new ChanceToARandomScreen(getActivity()));
+        mButtonDoorTwo.setOnClickListener(new ChanceToARandomScreen(getActivity()));
+
+    }
+
+    private void getWidgets() {
+        mButtonDoorOne = (Button) getView().findViewById(R.id.button_door_one);
+        mButtonDoorTwo = (Button) getView().findViewById(R.id.button_door_two);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
